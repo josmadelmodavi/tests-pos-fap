462dafed0c6f5f435e0df93c284a2fdc35fa5e61
Josmadelmo Davi <josmadelmodavi@gmail.com>
2020-06-28 16:00:27 -0300

    Erro em atrás de erro

---
+++ src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+  <h1>Login</h1>
+  <!-- <p v-show="isEmailInvalid" class="form__p_error_message">Error: Email {{fullname}} is not valid</p> -->
+  <!-- <p v-show="isLoginCompleted" class="form__p_error_message">Error: Password is less than 6 characters</p> -->
+  <div>
+    <label>Email:</label><br />
+    <input v-model="email" class="form__input_email" type="email" name="formEmail" autofocus="autofocus"><br /><br />
+  </div>
+  <div>
+    <label>Password:</label><br />
+    <input v-model="password" class="form__input_password" type="password" name="formPassword"><br /><br />
+  </div>
+  <button @click="goToUserForm" class="form__button_go_user_form">Login</button>
+</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+
+  computed: {
+    isLoginCompleted() {
+      const {
+        isValidEmail,
+        isValidPassword,
+      } = this;
+
+      return isValidEmail(email) && isValidPassword(password);
+    },
+  },
+
+  methods: {
+    // isValidEmail(string) {
+    //   const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    //   return !!string.match(emailRegex);
+    // },
+    isValidPassword(string) {
+      return string.length > 6;
+    },
+    goToUserForm() {
+      this.$router.push({ name: 'UserForm' });
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import Form from '../components/Form.vue';
+import Login from '../components/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -12,9 +12,9 @@ const routes = [
     component: Home,
   },
   {
-    path: '/form',
-    name: 'Form',
-    component: Form,
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
   {
     path: '/about',
