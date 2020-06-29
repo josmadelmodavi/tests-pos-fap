b504eda1bb9d0b247d792eac56601e1c31e0e2ac
Josmadelmo Davi <josmadelmodavi@gmail.com>
2020-06-29 19:59:23 -0300

    Ajuste na exibição de erro

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <h1>Login</h1>
-    <p v-show="!isEmailError" class="form__p_error_message">
-      Error: <u>{{email}}</u> is not valid email</p>
+    <p v-show="!isEmailOk" class="form__p_error_message">
+      Note: enter a valid email
+    </p>
     <p v-show="!isPasswordError" class="form__p_error_message">
-      Error: Password is less than 6 characters</p>
+      Note: enter a password longer than 6 characters
+    </p>
     <!-- <p v-show="isInvalidAccount" class="form__p_error_message">
       Error: This account does not exist</p> -->
     <div>
@@ -15,6 +17,7 @@
         type="email"
         name="formEmail"
         autofocus="autofocus"
+        placeholder="mail@example.com"
       /><br /><br />
     </div>
     <div>
@@ -24,6 +27,7 @@
         class="form__input_password"
         type="password"
         name="formPassword"
+        placeholder="Enter password"
       /><br /><br />
     </div>
     <button @click="goToUserForm" class="form__button_go_user_form">Login</button>
@@ -38,9 +42,9 @@ export default {
   }),
 
   computed: {
-    isEmailError() {
-      const { isValidEmail, email } = this;
-      return isValidEmail(email);
+    isEmailOk() {
+      const { isValidEmail, email, isMinStringEmail } = this;
+      return isValidEmail(email) && isMinStringEmail(email);
     },
     isPasswordError() {
       const { isValidPassword, password } = this;
@@ -55,11 +59,14 @@ export default {
 
   methods: {
     isValidEmail(string) {
-      const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+      const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/; // https://www.w3resource.com/javascript/form/email-validation.php
       return !!string.match(emailRegex);
     },
     isValidPassword(string) {
       return string.length >= 6;
+    },
+    isMinStringEmail(string) {
+      return string.length > 6;
     },
     goToUserForm() {
       this.$router.push({ name: 'UserForm' });
@@ -69,7 +76,8 @@ export default {
 </script>
 
 <style>
-.form__p_error_message, .form__p_error_message {
-  color:brown;
+.form__p_error_message,
+.form__p_error_message {
+  color: dimgrey;
 }
 </style>
